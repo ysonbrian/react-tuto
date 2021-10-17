b6659901585fce6c89f93f48bb168fa27f3ec84f
ysonbrian <znlsakxls@gmail.com>
2021-10-17 23:10:15 +0900

    Parallel Requests

--- src/GithubUser/RepoMenu.js
+++ src/GithubUser/RepoMenu.js
@@ -1,13 +1,14 @@
 import React, { useEffect } from 'react';
 import { useIterator } from '../CustomHooks/useIterator';
+// import RepositoryReadme from './RepositoryReadme';
 
-export function RepoMenu({ repositories, onSelect = (f) => f }) {
+export function RepoMenu({ repositories, login, onSelect = (f) => f }) {
   const [{ name }, previous, next] = useIterator(repositories);
 
-  useEffect(() => {
-    if (!name) return;
-    onSelect(name);
-  }, [name]);
+  // useEffect(() => {
+  //   if (!name) return;
+  //   onSelect(name);
+  // }, [name]);
 
   return (
     <div style={{ display: 'flex' }}>
@@ -17,3 +18,24 @@ export function RepoMenu({ repositories, onSelect = (f) => f }) {
     </div>
   );
 }
+
+/* <RepositoryReadme login={login} repo={name} /> */
+
+// import React from 'react';
+// import { useIterator } from './hooks';
+// import RepositoryReadme from './RepositoryReadme';
+
+// export default function RepoMenu({ repositories, login }) {
+//   const [{ name }, previous, next] = useIterator(repositories);
+
+//   return (
+//     <div>
+//       <div style={{ display: 'flex' }}>
+//         <button onClick={previous}>&lt;</button>
+//         <p>{name}</p>
+//         <button onClick={next}>&gt;</button>
+//       </div>
+//       <RepositoryReadme login={login} repo={name} />
+//     </div>
+//   );
+// }
