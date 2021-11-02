9c67d80a9b38b7820dbca6626bdfb7eecdd964f8
ysonbrian <znlsakxls@gmail.com>
2021-11-02 16:51:57 +0900

    State Props

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+
+const ExpenseItem = ({ date, title, amount }) => {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
+      </div>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
